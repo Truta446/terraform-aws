bfce09383190d54686fe6f36a6613e5c319e2f41
Juan Versolato Lopes <juan.versolato@outlook.com>
2023-05-20 10:10:33 -0300

    Terraform course.

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "juanversolato"
+
+    workspaces {
+      name = "example-workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a695f0d95cefc163"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
